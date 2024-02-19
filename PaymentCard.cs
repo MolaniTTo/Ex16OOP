2f6871c9408e9b7139a55d7bd772d830885a4166
arnauitb <arnau.molano.7e7@itb.cat>
2024-02-19 12:40:42 +0100

    Add project files.

---
+++ PaymentCard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex16OOP
+{
+    public class PaymentCard
+    {
+        private double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+
+        public PaymentCard(double openingBalance)
+        {
+            Balance = openingBalance;
+        }
+
+        public override string ToString()
+        {
+            return "The card has a balance of " + Balance + " pounds";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Ex16OOP;
+namespace Ex16OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentCard card = new PaymentCard(50);
+            Console.WriteLine(card);
+        }
+    }
+}
